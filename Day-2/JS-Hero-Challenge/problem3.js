a30b2149ed70bb089686f973731f5f2a3b7e327d
Panchajanya <utkarshprogrammer360@gmail.com>
2025-07-11 06:09:56 -0700

    Delete Day-2/JS-Hero-Challenge directory

--- Day-2/JS-Hero-Challenge/problem3.js
+++
@@ -1,8 +0,0 @@
-// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
-
-function lessThanOrEqualToZero(num) {
-	return num <= 0 ? true : false;
-}
-
-let output = lessThanOrEqualToZero(0);
-console.log(output);
